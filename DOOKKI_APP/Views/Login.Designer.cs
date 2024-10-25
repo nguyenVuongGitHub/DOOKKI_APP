7324470a67ca7440a13ab847047945e0c354619a
Vương Nguyễn Đình <nguyenvuong171104@gmail.com>
2024-10-25 14:03:45 +0700

    feat: press enter to login

--- DOOKKI_APP/Views/Login.Designer.cs
+++ DOOKKI_APP/Views/Login.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            label1 = new Label();
-            label2 = new Label();
-            label3 = new Label();
-            txtUserName = new TextBox();
-            txtPassword = new TextBox();
             btnLogin = new Button();
+            txtPassword = new TextBox();
+            txtUserName = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -51,53 +51,6 @@
             panel1.Size = new Size(492, 299);
             panel1.TabIndex = 0;
             // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
-            label1.Location = new Point(142, 13);
-            label1.Name = "label1";
-            label1.Size = new Size(217, 45);
-            label1.TabIndex = 0;
-            label1.Text = "ĐĂNG NHẬP";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 16F);
-            label2.Location = new Point(3, 87);
-            label2.Name = "label2";
-            label2.Size = new Size(156, 30);
-            label2.TabIndex = 1;
-            label2.Text = "Tên đăng nhập";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 16F);
-            label3.Location = new Point(3, 161);
-            label3.Name = "label3";
-            label3.Size = new Size(103, 30);
-            label3.TabIndex = 2;
-            label3.Text = "Mật khẩu";
-            // 
-            // txtUserName
-            // 
-            txtUserName.Font = new Font("Segoe UI", 16F);
-            txtUserName.Location = new Point(213, 87);
-            txtUserName.Name = "txtUserName";
-            txtUserName.Size = new Size(233, 36);
-            txtUserName.TabIndex = 3;
-            // 
-            // txtPassword
-            // 
-            txtPassword.Font = new Font("Segoe UI", 16F);
-            txtPassword.Location = new Point(213, 158);
-            txtPassword.Name = "txtPassword";
-            txtPassword.PasswordChar = '*';
-            txtPassword.Size = new Size(233, 36);
-            txtPassword.TabIndex = 4;
-            // 
             // btnLogin
             // 
             btnLogin.AutoSize = true;
@@ -110,15 +63,64 @@
             btnLogin.UseVisualStyleBackColor = true;
             btnLogin.Click += btnLogin_Click;
             // 
+            // txtPassword
+            // 
+            txtPassword.Font = new Font("Segoe UI", 16F);
+            txtPassword.Location = new Point(213, 158);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(233, 36);
+            txtPassword.TabIndex = 4;
+            // 
+            // txtUserName
+            // 
+            txtUserName.Font = new Font("Segoe UI", 16F);
+            txtUserName.Location = new Point(213, 87);
+            txtUserName.Name = "txtUserName";
+            txtUserName.Size = new Size(233, 36);
+            txtUserName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16F);
+            label3.Location = new Point(3, 161);
+            label3.Name = "label3";
+            label3.Size = new Size(103, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Mật khẩu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16F);
+            label2.Location = new Point(3, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(156, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Tên đăng nhập";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 24F, FontStyle.Bold);
+            label1.Location = new Point(142, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 45);
+            label1.TabIndex = 0;
+            label1.Text = "ĐĂNG NHẬP";
+            // 
             // Login
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(812, 460);
             Controls.Add(panel1);
+            KeyPreview = true;
             Name = "Login";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Login";
+            KeyDown += Login_KeyDown;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
