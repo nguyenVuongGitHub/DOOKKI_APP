dd0e0945b58062e324d22d8247bee8d7d53f9874
Vương Nguyễn Đình <nguyenvuong171104@gmail.com>
2024-10-27 14:16:24 +0700

    add: role user

---
+++ DOOKKI_APP/Helpers/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOOKKI_APP.Helpers
+{
+    public enum Roles
+    {
+        admin,
+        cashier
+    }
+    internal class User
+    {
+        private static Roles role;
+        public static Roles Role { get => role; private set => role = value; }
+        public static void SetRoles(string role)
+        {
+            switch (role.ToLower())
+            {
+                case "admin":
+                    Role = Roles.admin;
+                    break;
+                case "cashier":
+                    Role = Roles.cashier;
+                    break;
+                default:
+                    throw new ArgumentException("Invalid role provided.");
+
+            }
+        }
+    }
+}
