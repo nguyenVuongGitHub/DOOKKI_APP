484c1c46d26b3de6ace81b1f44375c4255b5783f
Vương Nguyễn Đình <nguyenvuong171104@gmail.com>
2024-11-06 21:48:28 +0700

    nothing

--- DOOKKI_APP/Views/InputCustomer.Designer.cs
+++ DOOKKI_APP/Views/InputCustomer.Designer.cs
@@ -32,11 +32,14 @@
             label1 = new Label();
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            textBox2 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(139, 53);
+            textBox1.Location = new Point(139, 12);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(159, 23);
             textBox1.TabIndex = 0;
@@ -44,7 +47,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(38, 56);
+            label1.Location = new Point(37, 15);
             label1.Name = "label1";
             label1.Size = new Size(27, 15);
             label1.TabIndex = 1;
@@ -52,7 +55,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(61, 111);
+            button1.Location = new Point(37, 148);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 2;
@@ -61,18 +64,46 @@
             // 
             // button2
             // 
-            button2.Location = new Point(223, 111);
+            button2.Location = new Point(139, 148);
             button2.Name = "button2";
             button2.Size = new Size(75, 23);
             button2.TabIndex = 3;
             button2.Text = "Hủy";
             button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            button3.Location = new Point(262, 137);
+            button3.Name = "button3";
+            button3.Size = new Size(106, 44);
+            button3.TabIndex = 4;
+            button3.Text = "Tao tai khoan";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(37, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(40, 15);
+            label2.TabIndex = 5;
+            label2.Text = "ten kh";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(139, 68);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(159, 23);
+            textBox2.TabIndex = 6;
+            // 
             // InputCustomer
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(374, 158);
+            ClientSize = new Size(415, 219);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(label1);
@@ -89,5 +120,8 @@
         private Label label1;
         private Button button1;
         private Button button2;
+        private Button button3;
+        private Label label2;
+        private TextBox textBox2;
     }
 }
